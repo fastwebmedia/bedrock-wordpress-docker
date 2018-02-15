754c8b0772c9917062d74d5d3f178917b7246f6b
Dan Lawson <daniel.lawson@fastwebmedia.com>
2018-02-15 15:26:01 +0000

    composer update as we already have core installed

--- fpm-wordpress/init.sh
+++ fpm-wordpress/init.sh
@@ -11,17 +11,4 @@ while ! nc -z ${DB_HOST} ${DB_PORT}; do
 	sleep 2
 done
 
-if ! $(wp core is-installed); then
-	if [ -n "$ADMIN_USER" ] && [ -n "$ADMIN_PASS" ] && [ -n "$ADMIN_EMAIL" ] && [ -n "$SITE_TITLE" ]; then
-		echo "You've given me install info. I guess I should use it. Installing..."
-		sync # see: https://github.com/docker/docker/issues/9547
-
-		wp core install \
-			--title=${SITE_TITLE} \
-			--admin_user=${ADMIN_USER} \
-			--admin_password=${ADMIN_PASS} \
-			--admin_email=${ADMIN_EMAIL} \
-			--url=${WP_HOME} \
-			--skip-email
-	fi
-fi
+composer install --no-interaction --no-scripts --no-autoloader
