1c1d39e264c28a992ab2c8b0b93c16d25fcc9f93
Dan Lawson <daniel.lawson@fastwebmedia.com>
2018-02-20 09:54:18 +0000

    Replace port

--- nginx-wordpress/init.sh
+++ nginx-wordpress/init.sh
@@ -1,5 +1,7 @@
 #!/bin/sh
 
+sed -i "s|listen 85|listen ${NGINX_PORT}|g" /etc/nginx/sites-available/default.conf
+
 if [ -n "$ALLOW_INSTALL" ] && [ "$ALLOW_INSTALL" == "yes" ]; then
 	sed -i "s|auth_basic|# auth_basic|g" /etc/nginx/sites-enabled/default.conf
 	sed -i "s|auth_basic_user_file|# auth_basic_user_file|g" /etc/nginx/sites-enabled/default.conf
